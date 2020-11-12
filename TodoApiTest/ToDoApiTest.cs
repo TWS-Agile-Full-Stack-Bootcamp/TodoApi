7d6b45bbfbd3628085992a10825025bb58acccd9
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-12 14:06:40 +0800

    test: add integration test Should_return_ok_and_todos_when_get_all_todos

--- TodoApiTest/ToDoApiTest.cs
+++ TodoApiTest/ToDoApiTest.cs
@@ -23,39 +23,34 @@ namespace TodoApiTest
         }
 
         [Fact]
-        public async Task Should_get_todos_from_mock_repository()
+        public async Task Should_return_ok_and_todos_when_get_all_todos()
         {
+            // given
             var mockIToDoRepository = new Mock<ITodoRepository>();
-            List<Todo> todos = new List<Todo>();
-            todos.Add(new Todo()
+            List<Todo> todos = new List<Todo>()
             {
-                Completed = false,
-                Id = 1,
-                Order = 1,
-                Title = "Mock ToDo"
-            });
+                new Todo(id: 1, title: "Mock ToDo", completed: false, order: 0),
+            };
             mockIToDoRepository.Setup(m => m.GetAll()).Returns(todos);
-            // Arrange
+
             var client = Factory.WithWebHostBuilder(builder =>
                 {
                     builder.ConfigureServices(services =>
                     {
-                        var serviceProvider = services.BuildServiceProvider();
-
                         services.AddScoped<ITodoRepository>((serviceProvider) => { return mockIToDoRepository.Object; });
                     });
                 })
-                .CreateClient(new WebApplicationFactoryClientOptions
-                {
-                    AllowAutoRedirect = false
-                });
+                .CreateClient();
 
+            // when
             var returnToDos = await client.GetAsync("/api/todo");
-            var responseBody = await returnToDos.Content.ReadAsStringAsync();
-            var todo = JsonConvert.DeserializeObject<List<Todo>>(responseBody);
 
-            Assert.False(todo.First().Completed);
-            Assert.Equal("Mock ToDo",todo.First().Title);
+            // then
+            var responseBody = await returnToDos.Content.ReadAsStringAsync();
+            var actualTodos = JsonConvert.DeserializeObject<List<Todo>>(responseBody);
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, returnToDos.StatusCode);
+            Assert.Equal(todos, actualTodos);
         }
     }
 }

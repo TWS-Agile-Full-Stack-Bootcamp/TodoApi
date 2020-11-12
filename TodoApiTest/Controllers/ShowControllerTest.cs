30d093e701132687d0dc17d4ae5b774abf3c98f0
Wu Xueheng <xhwu@thoughtworks.com>
2020-11-12 08:00:34 +0800

    feat: add controller test demo

--- TodoApiTest/Controllers/ShowControllerTest.cs
+++ TodoApiTest/Controllers/ShowControllerTest.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TodoApi.Controllers;
+using TodoApi.Services;
+using Xunit;
+
 namespace TodoApiTest.Controllers
 {
     public class ShowControllerTest
     {
-        public ShowControllerTest()
+        [Fact]
+        public void Should_return_expected_message_when_GetAll_successfully()
         {
+            // given
+            string expectedMessage = "a beauty tag";
+            var mockService = new Mock<ShowService>(null);
+            mockService.Setup(service => service.GetShowLabel())
+                .Returns(expectedMessage);
+            var showController = new ShowController(mockService.Object);
+
+            // when
+            ObjectResult actionResult = showController.GetAll() as ObjectResult;
+
+            // then
+            Assert.Equal(200, actionResult.StatusCode);
+            Assert.Equal(expectedMessage, actionResult.Value);
         }
     }
 }
